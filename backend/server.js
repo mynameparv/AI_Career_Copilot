150b2e459461da54190e034508e9fffacbd7c4f5
mynameparv <parvajain327p@gmail.com>
2026-02-06 00:05:47 +0530

    new frontend deployed

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ app.use(cors({
     origin: [
         'http://localhost:3000',
         'http://localhost:5173',
-        'https://careerhelpprojects-parva-jains-projects.vercel.app'
+        'https://ai-career-copilot-azure.vercel.app/'
     ],
     credentials: true
 }));
